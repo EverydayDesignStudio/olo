7fdcd2faa5ccd024ee8242ba62a2ec78e7e99631
Tal <everydaydesignstudio@gmail.com>
2018-04-10 17:43:21 -0700

    delete screenshot

--- cleanup.py
+++ cleanup.py
@@ -5,6 +5,7 @@ gpio.setmode(gpio.BOARD)
 gpio.setup(31, gpio.OUT) #gpio 6  - motor driver enable 
 gpio.setup(33, gpio.OUT) #gpio 13 - motor driver direction 1
 gpio.setup(32, gpio.OUT) #gpio 12 - motor driver direction 2
+gpio.output(31, True)
 gpio.output(32, False)
 gpio.output(33, False)
 gpio.cleanup()
