a3dc5bc1e791a544599e1041b51cdd44cc1b18a2
Tal Amram <tal89amram@gmail.com>
2018-04-24 16:22:19 -0700

    gpio numbers

--- pintest.py
+++ pintest.py
@@ -1,9 +1,9 @@
 import time
 import RPi.GPIO as gpio
 gpio.setmode(gpio.BOARD)
-gpio.setup(26, gpio.IN)
+gpio.setup(37, gpio.IN)
 
 while(True):
-    print(gpio.input(26))
+    print(gpio.input(37))
     time.sleep(0.4)
     print('-')
