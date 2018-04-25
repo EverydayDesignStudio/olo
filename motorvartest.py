f919c155c2abfd908c7c383b5dac4c3cd36ff657
Tal Amram <tal89amram@gmail.com>
2018-04-25 14:27:59 -0700

    mcp in olofunctions

--- motorvartest.py
+++ motorvartest.py
@@ -29,12 +29,7 @@ import RPi.GPIO as gpio
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-# Software SPI configuration:
-CLK  = 11
-MISO = 9
-MOSI = 10
-CS   = 8
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
 
 # GPIO configuration:
 #gpio.setmode(gpio.BOARD)
--- oloFunctions.py
+++ oloFunctions.py
@@ -1,3 +1,10 @@
+# Software SPI configuration:
+CLK  = 11
+MISO = 9
+MOSI = 10
+CS   = 8
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
 def readValues():
     # Read all the ADC channel values in a list.
     values = [0]*8
@@ -5,3 +12,11 @@ def readValues():
         # The read_adc function will get the value of the specified channel (0-7).
         values[i] = mcp.read_adc_difference(i)
     return values
+
+def printValues(vals):
+    print('=' * 57)
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+    print('-' * 57)
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*vals))
+    # Pause for half a second.
+    time.sleep(0.5)
