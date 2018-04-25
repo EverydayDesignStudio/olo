ec7347b9380a5a6fb0a4081b237c7296597e849b
Tal Amram <tal89amram@gmail.com>
2018-04-24 17:16:15 -0700

    turn off motor

--- motorvartest.py
+++ motorvartest.py
@@ -85,20 +85,32 @@ while(True):
     # Pause for half a second.
     time.sleep(0.5)
     target = int(raw_input("where to, captain? "))
-
+    prev = '<>'
     values = readValues()
 
     while (abs(values[0] - target) > 10):
+
         print('motor loop')
         print('target: ' + str(target) + '  current: ' + str(values[0]))
         print ('difference:' + str(abs(values[0] - target)))
         if values[0] > target:
             print('>>>')
-            gpio.output(32, True)
-            gpio.output(33, False)
+            if prev = '>>>':
+                pass
+            else:
+                gpio.output(32, True)
+                gpio.output(33, False)
+                prev = '>>>'
         if values[0] < target:
             print('<<<')
-            gpio.output(32, False)
-            gpio.output(33, True)
+            if prev = '<<<':
+                pass
+            else:
+                prev = '<<<'
+                gpio.output(32, False)
+                gpio.output(33, True)
         time.sleep(1)
         values = readValues()
+
+    gpio.output(32, False)
+    gpio.output(33, False)
