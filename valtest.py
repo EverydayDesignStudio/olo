61cd3c41dba1580a7092cad3619c760caebd3867
Tal Amram <tal89amram@gmail.com>
2018-04-30 18:02:02 -0700

    testprint

--- valtest.py
+++ valtest.py
@@ -28,22 +28,12 @@ from oloFunctions import *
 import conf
 
 conf.init()
-
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+print sh.CLK
+mcp = Adafruit_MCP3008.MCP3008(clk=sh.CLK, cs=sh.CS, miso=sh.MISO, mosi=sh.MOSI)
 
 # GPIO configuration:
-gpio.setup(31, gpio.OUT) #gpio 6  - motor driver enable
-gpio.setup(33, gpio.OUT) #gpio 13 - motor driver direction 1
-gpio.setup(32, gpio.OUT) #gpio 12 - motor driver direction 2
-
-gpio.setup(switch1, gpio.IN) #gpio 16  - three pole switch 1
-gpio.setup(switch2, gpio.IN) #gpio 18  - three pole switch 2
-
-# Enable motor driver, turn off other outputs:
-gpio.output(31, True)
-gpio.output(32, False)
-gpio.output(33, False)
-
+gpio.setup(sh.switch1, gpio.IN) #gpio 16  - three pole switch 1
+gpio.setup(sh.switch2, gpio.IN) #gpio 18  - three pole switch 2
 
 print('Reading MCP3008 values, press Ctrl-C to quit...')
 
