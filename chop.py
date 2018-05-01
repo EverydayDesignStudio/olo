6e03e92a25381d1f0a422d700fed2a30c0b08d84
Tal Amram <tal89amram@gmail.com>
2018-05-01 16:51:49 -0700

    choip chopc hop

---
+++ chop.py
@@ -0,0 +1,14 @@
+
+
+import csv
+import time
+import datetime
+import sh
+
+segments = 100
+
+
+def convertTimestamp(tstamp):
+    print datetime.datetime.fromtimestamp(int(tstamp)
+
+convertTimestamp(time.now())    
