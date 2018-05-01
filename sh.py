9bdfef1aaa617cdb1c9ba68a2c43ddfc17794e06
Tal Amram <tal89amram@gmail.com>
2018-04-30 17:56:45 -0700

    tab comments

--- sh.py
+++ sh.py
@@ -1,18 +1,18 @@
 # oloGlobals
 
 def init():
-# pin configuration
+    # pin configuration
     global switch1 = 23
     global switch2 = 24
     global mEnable = 6
     global mLeft = 12
     global mRight = 13
 
-# Software SPI configuration:
+    # Software SPI configuration:
     global CLK  = 11
     global MISO = 9
     global MOSI = 10
     global CS   = 8
 
-# pretty labels
+    # pretty labels
     global labels = ['swi', 'cap', 'sw1', 'sw2']
