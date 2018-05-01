e0534783bce9609a756ac8929c7bcaa4dedc7921
Tal Amram <tal89amram@gmail.com>
2018-05-01 14:18:05 -0700

    no more sleep

--- valtest.py
+++ valtest.py
@@ -62,4 +62,4 @@ while(True):
     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
     """
     # Pause for half a second.
-    time.sleep(0.5)
+    #time.sleep(0.5)
