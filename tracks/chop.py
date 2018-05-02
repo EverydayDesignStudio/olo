a7b4a26b46cf5c27f40e383c8e1ae70ab6c880b9
Tal Amram <tal89amram@gmail.com>
2018-05-01 17:02:09 -0700

    chop.

---
+++ tracks/chop.py
@@ -0,0 +1,27 @@
+
+
+import csv
+import time
+import datetime
+import sh
+
+segments = 100
+
+
+
+def convertTimestamp(tstamp):
+    print datetime.datetime.fromtimestamp(int(tstamp))
+
+convertTimestamp(time.time())
+
+
+
+with open('exported_tracks.txt','rb') as tsvin:
+    tsvin = csv.reader(tsvin, delimiter = '\t' )
+    rowCount = sum(1 for row in tsvin)
+    print('rowCount: ' + str(rowCount))
+
+    index = 0
+    fileName = str(index) + '_life.csv'
+    for row in range(rowCount):
+        print(tsvin.row[1])
