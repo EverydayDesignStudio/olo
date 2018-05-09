bbf75a7e3c9213783c93b983e7ded299c1cdc822
Tal Amram <tal89amram@gmail.com>
2018-05-09 14:57:07 -0700

    1

--- csvtest.py
+++ csvtest.py
@@ -147,7 +147,7 @@ if togglesort:
 # =====================================================
 # LIFE TIMEFRAME
 print col.und + 'LIFE SUBLISTS' + col.none
-path = 'tracks/life/
+path = 'tracks/life/'
 with open(str.split(lifename, '.txt')[0] + '_life.txt', 'r') as rl:
     reader = csv.reader(rl, delimiter ='\t')
     rows = sum(1 for row in reader)
@@ -163,7 +163,7 @@ with open(str.split(lifename, '.txt')[0] + '_life.txt', 'r') as rl:
 
 # YEAR TIMEFRAME
 print col.und + 'YEAR SUBLISTS' + col.none
-path = 'tracks/year/
+path = 'tracks/year/'
 with open(str.split(yearname, '.txt')[0] + '_year.txt', 'r') as rl:
     reader = csv.reader(rl, delimiter ='\t')
     rows = sum(1 for row in reader)
@@ -178,7 +178,7 @@ with open(str.split(yearname, '.txt')[0] + '_year.txt', 'r') as rl:
 
 # DAY TIMEFRAME
 print col.und + 'DAY SUBLISTS' + col.none
-path = 'tracks/day/
+path = 'tracks/day/'
 with open(str.split(dayname, '.txt')[0] + '_day.txt', 'r') as rl:
     reader = csv.reader(rl, delimiter ='\t')
     rows = sum(1 for row in reader)
