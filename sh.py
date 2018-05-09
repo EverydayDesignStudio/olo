1226c4f9150d9386377d555e5722235a089cdd99
Tal Amram <tal89amram@gmail.com>
2018-05-09 15:10:29 -0700

    1

--- sh.py
+++ sh.py
@@ -1,4 +1,13 @@
 # oloGlobals
+class col:
+    prp = '\033[95m'
+    vio = '\033[94m'
+    gre = '\033[92m'
+    yel = '\033[93m'
+    ora = '\033[91m'
+    none = '\033[0m'
+    red = '\033[1m'
+    und = '\033[4m'
 
 def init():
     # pin configuration
@@ -22,6 +31,8 @@ def init():
     MOSI = 10
     global CS
     CS   = 8
+    global yesorno
+    yesorno = col.none + '[ ' + col.gre + 'Y' + col.none + ' / ' + col.red + 'N' + col.none + " ] "
 
     global values
 
