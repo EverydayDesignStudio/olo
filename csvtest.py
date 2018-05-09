51e992a0b62bdee929275366172c02bd9928fe69
Tal Amram <tal89amram@gmail.com>
2018-05-09 16:14:17 -0700

    input resolution

--- csvtest.py
+++ csvtest.py
@@ -11,7 +11,7 @@ import datetime
 import calendar
 import time
 import sh
-resolution = 20
+resolution = int(raw_input('resolution? '))
 
 filename = 'tracks/exported_tracks.txt'
 lifename = str.split(filename, '.txt')[0] + '_life.txt'
