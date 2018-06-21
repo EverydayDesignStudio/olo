a3c72338bd79a6677f3defd9453e5155fb858463
Tal Amram <tal89amram@gmail.com>
2018-06-20 17:32:41 -0700

    pwm on olo

--- controlTest.py
+++ controlTest.py
@@ -85,6 +85,9 @@ gpio.setup(sh.mEnable, gpio.OUT) #gpio 6  - motor driver enable
 gpio.setup(sh.mLeft, gpio.OUT) #gpio 13 - motor driver direction 1
 gpio.setup(sh.mRight, gpio.OUT) #gpio 12 - motor driver direction 2
 
+leftpwm = gpio.PWM(sh.mLeft, 100)
+rightpwm = gpio.PWM(sh.mRight, 100)
+
 gpio.setup(sh.switch1, gpio.IN) #gpio 16  - three pole switch 1
 gpio.setup(sh.switch2, gpio.IN) #gpio 18  - three pole switch 2
 
@@ -114,6 +117,11 @@ while(True):
     if target < 0:
         readValues()
         print sh.values[wi_channel]
+    if target > 2000:
+        for dc in range(0, 101, 5):      # Loop from 0 to 100 stepping dc up by 5 each loop
+        pwm.ChangeDutyCycle(dc)
+        time.sleep(0.05)               # wait for .05 seconds at current LED brightness level
+        print(dc)    
     else:
 
 
