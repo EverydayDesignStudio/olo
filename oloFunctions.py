0f20be4d8c115fc3104d76f4a5ee9ed42850ef2c
Tal Amram <tal89amram@gmail.com>
2018-10-01 14:05:04 -0700

    print all vals

--- oloFunctions.py
+++ oloFunctions.py
@@ -100,12 +100,9 @@ def readValues():
 
 
 def printValues(vals):
-    newVals = [0] * 4
-    for i in range(4):
+    newVals = [0] * 8
+    for i in range(8):
         newVals[i] = vals[i]
-    newVals[0] = vals[4]
-    #print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*vals))
-    #print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} |'.format(*newVals))
     print(newVals[0])
     # Pause for half a second.
     #time.sleep(0.5)
