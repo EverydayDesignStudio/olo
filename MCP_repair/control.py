b1b57fbe0eb43423d9d0be8e9d082aafbd1868e7
Tal Amram <tal89amram@gmail.com>
2018-10-01 11:55:04 -0700

    1

--- MCP_repair/control.py
+++ MCP_repair/control.py
@@ -7,7 +7,8 @@ import time
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
-
+import sh
+sh.init()
 
 # Software SPI configuration:
 CLK  = 11
@@ -22,18 +23,32 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 # mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
-print('Reading MCP3008 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-print('-' * 57)
-# Main program loop.
-while True:
+# print('Reading MCP3008 values, press Ctrl-C to quit...')
+# # Print nice channel column headers.
+# print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+# print('-' * 57)
+# # Main program loop.
+# while True:
+#     # Read all the ADC channel values in a list.
+#     values = [0]*8
+#     for i in range(8):
+#         # The read_adc function will get the value of the specified channel (0-7).
+#         values[i] = mcp.read_adc(i)
+#     # Print the ADC values.
+#     print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+#     # Pause for half a second.
+# time.sleep(0.5)
+
+
+
+
+
+def readValues():
     # Read all the ADC channel values in a list.
-    values = [0]*8
-    for i in range(8):
+    sh.values = [0]*8
+    for i in range(7):
         # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
-    # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    # Pause for half a second.
-time.sleep(0.5)
+        sh.values[i] = mcp.read_adc_difference(i)
+        # values[2] = gpio.input(sh.switch1) #when 3pole switch <--> GPIO 23
+        # values[3] = gpio.input(sh.switch2) #when 3pole switch <--> GPIO 24
+    return sh.values
