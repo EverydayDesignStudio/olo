8d21b9bc4912e73a8a466ca874e5c0b63e48e62b
Minyoung Yoo <yoomy1203@hotmail.com>
2019-03-07 16:50:42 -0800

    1

--- oloFunctions_updated.py
+++ oloFunctions_updated.py
@@ -33,7 +33,7 @@ try:
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
     # create the cs (chip select)
-    cs = digitalio.DigitalInOut(board.D5)
+    cs = digitalio.DigitalInOut(board.D8)
 
     # create the mcp object
     mcp = MCP.MCP3008(spi, cs)
@@ -104,14 +104,23 @@ def readValues():
     sh.values = [0]*8
 
     # The read_adc function will get the value of the specified channel (0-7).
-    sh.values[0] = AnalogIn(mcp, MCP.P0, MCP.P1).value
-    sh.values[1] = AnalogIn(mcp, MCP.P1, MCP.P0).value
-    sh.values[2] = AnalogIn(mcp, MCP.P2, MCP.P3).value
-    sh.values[3] = AnalogIn(mcp, MCP.P3, MCP.P2).value
-    sh.values[4] = AnalogIn(mcp, MCP.P4, MCP.P5).value
-    sh.values[5] = AnalogIn(mcp, MCP.P5, MCP.P4).value
-    sh.values[6] = AnalogIn(mcp, MCP.P6, MCP.P7).value
-    sh.values[7] = AnalogIn(mcp, MCP.P7, MCP.P6).value
+    ch0 = AnalogIn(mcp, MCP.P0, MCP.P1)
+    ch1 = AnalogIn(mcp, MCP.P1, MCP.P0)
+    ch2 = AnalogIn(mcp, MCP.P2, MCP.P3)
+    ch3 = AnalogIn(mcp, MCP.P3, MCP.P2)
+    ch4 = AnalogIn(mcp, MCP.P4, MCP.P5)
+    ch5 = AnalogIn(mcp, MCP.P5, MCP.P4)
+    ch6 = AnalogIn(mcp, MCP.P6, MCP.P7)
+    ch7 = AnalogIn(mcp, MCP.P7, MCP.P6)
+
+    sh.values[0] = ch0.value
+    sh.values[1] = ch1.value
+    sh.values[2] = ch2.value
+    sh.values[3] = ch3.value
+    sh.values[4] = ch4.value
+    sh.values[5] = ch5.value
+    sh.values[6] = ch6.value
+    sh.values[7] = ch7.value
 
     return sh.values
 
