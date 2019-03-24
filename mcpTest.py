653342879a39f51768afa759768ac050d8430e78
Minyoung Yoo <yoomy1203@hotmail.com>
2019-03-24 14:48:08 -0700

    Add MCP test

---
+++ mcpTest.py
@@ -0,0 +1,19 @@
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D8)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+while True:
+    chan = AnalogIn(mcp, MCP.P0).value >> 6
+    print('Raw ADC Value: ', chan)
