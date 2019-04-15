1f7593640741d0b7d6ef8772857a3f9f92fad119
Minyoung Yoo <yoomy1203@hotmail.com>
2019-04-15 15:31:06 -0700

    Create a controller that quits/starts the main script

---
+++ oloController.py
@@ -0,0 +1,6 @@
+import oloMain
+
+# TODO: isOn is off for 3 minutes, quit oloMain script
+
+# TODO: if PIN 17 is high, start oloMain script
+# https://stackoverflow.com/questions/7974849/how-can-i-make-one-python-file-run-another
